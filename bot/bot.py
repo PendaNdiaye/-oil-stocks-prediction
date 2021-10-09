7c0dc8536d9ee6b1aa15b0de6be477c3e9c1b704
PendaNdiaye <ndiayependa1992@gmail.com>
2021-10-09 21:10:22 +0000

    feat(prod):clean notebook

--- bot/bot.py
+++ bot/bot.py
@@ -3,8 +3,25 @@ from chromedriver_py import binary_path # this will get you the path variable
 import os
 import pandas as pd
 
+import hashlib
+import requests
+#from selenium import webdriver
+from datetime import datetime
+import psycopg2
+from settings import (
+    CONFIG_PATH,   
+)
+from exceptions import (
+    RegisterModeException,
+    EmptyNewsPageException,
+)
+from docker_env import is_dot_docker_env_there
+import yaml
+from tools import normalize_date
+from config import get_driver
+
 REPO_DIR = os.path.dirname(os.path.abspath(''))
-BASE_URL = "https://fr.investing.com/commodities/crude-oil-news"
+BASE_URL = "https://www.investing.com/crypto/bitcoin"
 DATA_DIR = os.path.join(REPO_DIR, "data/")
 
 FILE_PATH = os.path.join(DATA_DIR, "articles/output.csv")
@@ -19,46 +36,84 @@ def load_configs():
     #return chrome_driver, user_agent
     pass 
 
-def get_driver():
+#def get_driver():
     #configs = load_configs()
     #if isinstance(configs, tuple):
         # local env dev
     #chrome_driver_path, user_agent = configs
     #print(chrome_driver_path)
-    driver = webdriver.Chrome(binary_path)
-    return driver
+ #   driver = webdriver.Chrome(binary_path)
+  #  return driver
 
-def __get_titles(driver):
-        return [s.text for s in driver.find_elements_by_class_name("title")][3:13]
-    
-def __get_dates(driver):
-        return [s.text for  s in driver.find_elements_by_xpath('.//span[@class = "date"]')]
-    
-def format_string(s):
-    return s.replace(' - ','')
 
-def format_date(dates):
-    return list(map(format_string, dates))
 
-def scrape(url):
-    #url = base_url + '/'+ str(page)
-    driver = get_driver()
-    driver.get(url)
-    titles = __get_titles(driver)
-    dates = __get_dates(driver)
-    dates = format_date(dates)
-    return titles, dates
 
-def get_urls(base_url, pagination):
-    #urls = [os.path.join(BASE_URL, str(page)) for page in range(1, pagination+1)]
-    urls = [base_url + '/' + str(page) for page in range(1, pagination+1)]
-    return urls
+class GoldNewsRetriever:
 
-def scrape_iterate(urls):
-    titles = []
-    dates = []
-    for url in urls:
-        current_titles, current_dates = scrape(url)
-        titles += current_titles
-        dates +=current_dates
-    return titles, dates  
+    def __init__(self):
+        self.base_url = BASE_URL
+
+    def scrape(self, **kwargs):
+        driver, user_agent = get_driver()
+        url = self.base_url
+        current = True
+        if 'page' in kwargs:
+            current = False
+            page = kwargs.get('page')
+            url = os.path.join(url, str(page))
+            docker_env = is_dot_docker_env_there()
+            if docker_env:
+                response = requests.get(url=url)
+            else:
+                headers = {'User-Agent': user_agent}
+                response = requests.get(url=url, headers=headers)
+            if len(response.history) == 1:
+                #raise Exception("No data retrieved")
+                raise EmptyNewsPageException(page)
+        driver.get(url)
+        titles = self.__get_titles(driver)
+        dates = self.__get_dates(driver)
+        # process dates before push them to backend
+        norm_dates = [normalize_date(date) for date in dates]
+        dates = norm_dates
+        sources = self.__get_sources(driver)
+        hashes = [hashlib.md5(title.encode()).hexdigest() for title in titles]
+        chronos = [datetime.now().strftime("%m/%d/%Y, %H:%M:%S")] * len(hashes)
+        # return data as dict
+        scraped_data = []
+        for data in zip(
+            hashes,
+            chronos,
+            titles,
+            sources,
+            dates):
+            s = dict()
+            s['header_hash'], s['scraping_date'], s['new_header'], s['source'], s['public_date'] = data
+            scraped_data.append(s)
+        return scraped_data
+
+    def current_scraper(self):
+        return self.scrape()
+
+    def back_scraper(self, pagination_low, pagination_up):
+        lower = pagination_low
+        upper = pagination_up
+        history_data = []
+        for page in range(lower, upper):
+            print(page)
+            history_data += self.scrape(page=page)
+            print("ok")
+        return history_data
+
+
+    @staticmethod
+    def __get_titles(driver, limit=15):
+        return [s.text for s in driver.find_elements_by_class_name("title")[11:limit + 11]]
+
+    @staticmethod
+    def __get_dates(driver, limit=15):
+        return [s.text for  s in driver.find_elements_by_xpath('.//span[@class = "date"]')[:limit]]
+
+    @staticmethod
+    def __get_sources(driver, limit=15):
+        return [s.text for s in driver.find_elements_by_xpath('//span[contains(text(), "By")]')[:limit]]
