ddbd9ff31338faa273cc28bd07b760ccbd823a91
Elhadji Gagny Sylla <AJ1044996@tdf.hubtotal.net>
2021-10-17 14:43:20 +0200

    feat(fix bot): get out os dependencies

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -1,16 +1,33 @@
 import os
 
 
+
+def update_slash_depends_on_system(s):
+    import platform
+    slash = {
+        "Windows" : "\\",
+        "Darwin": "/"
+
+    }
+    platform__ = platform.system()
+    context_slash = slash.get(platform__)
+    
+    
+    context_s =  s.replace('/', context_slash)
+    return context_s
+
+    
 REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath('')))
 
 BASE_URL = "https://www.investing.com/crypto/bitcoin/news"
-DATA_DIR = os.path.join(REPO_DIR, "data/")
-FILE_PATH = os.path.join(DATA_DIR, "articles/output.csv")
+DATA_DIR = os.path.join(REPO_DIR, update_slash_depends_on_system("data/"))
+FILE_PATH = os.path.join(DATA_DIR, update_slash_depends_on_system("articles/output.csv"))
 
 
 # load chromedriver path from yaml
 import yaml
-with open(os.path.join(REPO_DIR, "bot/bot/chrome.yaml")) as file:
+with open(os.path.join(REPO_DIR, update_slash_depends_on_system("bot/bot/chrome.yaml"))) as file:
     chrome_config = yaml.full_load(file)
     CHROMEDRIVER_PATH= chrome_config['CHROME_DRIVER']
+
 
--- model/model/settings.py
+++ model/model/settings.py
@@ -1,8 +1,25 @@
 import os
 
+
+
+
+def update_slash_depends_on_system(s):
+    import platform
+    slash = {
+        "Windows" : "\\",
+        "Darwin": "/"
+
+    }
+    platform__ = platform.system()
+    context_slash = slash.get(platform__)
+    
+    
+    context_s =  s.replace('/', context_slash)
+    return context_s
+
 REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath('')))
-DATA_DIR = os.path.join(REPO_DIR, "data/")
-FILE_PATH = os.path.join(DATA_DIR, "articles/back.txt")
-HISTORY_PATH = os.path.join(DATA_DIR, "articles/history.csv")
+DATA_DIR = os.path.join(REPO_DIR, update_slash_depends_on_system("data/"))
+FILE_PATH = os.path.join(DATA_DIR, update_slash_depends_on_system("articles/back.txt"))
+HISTORY_PATH = os.path.join(DATA_DIR, update_slash_depends_on_system("articles/history.csv"))
 
 
