14ee5590bba9f0bac36b618256027dbf9bfffbe5
Elhadji Gagny Sylla <AJ1044996@tdf.hubtotal.net>
2021-10-17 14:04:39 +0200

    feat(build server): add files

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -1,11 +1,9 @@
 import os
 
 
-REPO_DIR = os.path.dirname(os.path.abspath(''))
-
+REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath('')))
 
 BASE_URL = "https://www.investing.com/crypto/bitcoin/news"
 DATA_DIR = os.path.join(REPO_DIR, "data/")
-
 FILE_PATH = os.path.join(DATA_DIR, "articles/output.csv")
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
---
+++ model/model/settings.py
@@ -0,0 +1,8 @@
+import os
+
+REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath('')))
+DATA_DIR = os.path.join(REPO_DIR, "data/")
+FILE_PATH = os.path.join(DATA_DIR, "articles/back.txt")
+HISTORY_PATH = os.path.join(DATA_DIR, "articles/history.csv")
+
+
