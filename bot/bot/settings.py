58f180b36bfcf5afb13c1ba53527788e9ea65aae
Elhadji Gagny Sylla <AJ1044996@tdf.hubtotal.net>
2021-10-17 15:13:45 +0200

    fix bugs

--- bot/bot/settings.py
+++ bot/bot/settings.py
@@ -17,8 +17,7 @@ def update_slash_depends_on_system(s):
     return context_s
 
     
-REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath('')))
-
+REPO_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 BASE_URL = "https://www.investing.com/crypto/bitcoin/news"
 DATA_DIR = os.path.join(REPO_DIR, update_slash_depends_on_system("data/"))
 FILE_PATH = os.path.join(DATA_DIR, update_slash_depends_on_system("articles/output.csv"))
@@ -30,4 +29,4 @@ with open(os.path.join(REPO_DIR, update_slash_depends_on_system("bot/bot/chrome.
     chrome_config = yaml.full_load(file)
     CHROMEDRIVER_PATH= chrome_config['CHROME_DRIVER']
 
-
+print(REPO_DIR)
